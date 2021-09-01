bd4cc7f87a219a7b61d1a2c2969fd7c02d2ceccf
marlon <751081818@qq.com>
2021-09-01 16:51:14 +0900

    Commmand Patterns

---
+++ 21-command-pattern/projects/starter/AdvancedDesignPatterns.xcworkspace/AdvancedDesignPatterns.playground/Pages/Command.xcplaygroundpage/Contents.swift
@@ -0,0 +1,93 @@
+/*:
+ [Previous](@previous)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[Next](@next)
+ 
+ # Command
+ - - - - - - - - - -
+ ![Command Diagram](Command_Diagram.png)
+ 
+ The command pattern is a behavioral pattern that encapsulates information to perform an action into a "command object." It involves three types:
+ 
+ 1. The **invoker** stores and executes commands.
+ 2. The **command** encapsulates the action as an object.
+ 3. The **receiver** is the object that's acted upon by the command.
+ 
+ ## Code Example
+ */
+import Foundation
+
+// receiver
+public class Door{
+	public var isOpen = false
+}
+
+
+//Command
+
+public class DoorCommand{
+	public let door: Door
+	
+	public init(_ door: Door){
+		self.door = door
+	}
+	
+	public func execute(){}
+}
+
+public class OpenCommand: DoorCommand{
+	public override func execute() {
+		print("openning the door")
+		door.isOpen = true
+	}
+}
+
+
+public class CloseCommand: DoorCommand{
+	public override func execute() {
+		print("closing the door...")
+		door.isOpen = false
+	}
+}
+
+
+
+public class Doorman{
+	public let commands: [DoorCommand]
+	public let door: Door
+	
+	public init(door: Door){
+		let commandCount = arc4random_uniform(10) + 1
+		self.commands = (0..<commandCount).map{ index in
+			return index % 2 == 0  ? OpenCommand(door) : CloseCommand(door)
+		}
+		
+		self.door = door
+	}
+	
+	public func execute(){
+		print("Doorman is ...")
+		commands.forEach {
+			$0.execute()
+		}
+	}
+}
+
+
+//example
+
+public let isOpen = true
+print("You predict the door will be" + "\(isOpen ? "open" : "closed" )")
+print(" ")
+
+
+let door = Door()
+let doorman = Doorman(door: door)
+doorman.execute()
+
+print("  ")
+
+if door.isOpen == isOpen {
+	print("You were right! :]")
+} else {
+	print("You were wrong :[")
+}
+print("The door is \(door.isOpen ? "open" : "closed").")
